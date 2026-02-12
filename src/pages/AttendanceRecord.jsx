5a9b9eab8b5da48c87d249b69daf5652475fb1e4
darshan ambaliya <darshanambaliya153@gmail.com>
2026-02-12 14:02:43 +0530

    backend host railway to render change

--- src/pages/AttendanceRecord.jsx
+++ src/pages/AttendanceRecord.jsx
@@ -15,7 +15,7 @@ export default function AttendanceRecord() {
   ];
   const API_BASE_URL = window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://mahakali-textiles-production.up.railway.app";
+    : "https://mahakali-textiles.onrender.com";
 
   const API_URL = `${API_BASE_URL}/api/employees`;
   useEffect(() => {
--- src/pages/ProductionReport/ProductionReport.jsx
+++ src/pages/ProductionReport/ProductionReport.jsx
@@ -12,7 +12,7 @@ const ProductionReport = () => {
 
   const API_BASE_URL = window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://mahakali-textiles-production.up.railway.app";
+    : "https://mahakali-textiles.onrender.com";
 
   const API_URL = `${API_BASE_URL}/api/production/`;
 
